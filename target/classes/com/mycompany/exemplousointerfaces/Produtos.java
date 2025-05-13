a27564f7babb37919884f3e20fb6e70de40758b8
Matheus Ramos <matheusfreitas@unifucamp.edu.br>
2025-05-13 10:50:43 -0300

    Update Produtos.java

--- target/classes/com/mycompany/exemplousointerfaces/Produtos.java
+++ target/classes/com/mycompany/exemplousointerfaces/Produtos.java
@@ -1,1 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.exemplousointerfaces;
 
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Servidor
+ */
+public class Produtos {
+    private final int ID;
+    private final String Nome;
+    private int Estoque;
+    
+    public Produtos(int ID, String Nome, int Estoque){
+        this.Estoque = Estoque;
+        this.Nome = Nome;
+        this.ID = ID;  
+    }
+    
+    public void Mostrar(){
+        System.out.println("ID: " + ID + "\nNome do Produto: " + Nome + "\nQuantidade em estoque: " + Estoque + "\n");
+    }
+}
